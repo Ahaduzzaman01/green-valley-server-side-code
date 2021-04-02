7d828464ebd36e9d6c41a758614f8630388682ba
Ahaduzzaman01 <abdulahad5161@gmail.com>
2021-04-02 16:24:14 +0600

    server side complete

---
+++ README.md
@@ -0,0 +1,18 @@
+# My website name is: Green Valley
+
+**Links**
+[Live Site Link]()
+
+[Client Side Code Link]()
+
+[Server Side Code Link]()
+
+## Key features of this website:
+1. This is full stack website,
+2. User can checkout,
+3. Users can see their orders,
+4. User can add product in website,
+5. User can remove or delete any product,
+6. Log in system added (Google Log In),
+7. Private route used in website,
+8. Good looking UI.
---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wvime.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productCollection = client.db("greenValley").collection("products");
+    const checkoutCollection = client.db("greenValley").collection("checkouts");
+
+    app.get('/products', (req, res) => {
+        productCollection.find()
+            .toArray((err, products) => {
+                res.send(products)
+            })
+    })
+
+    app.post('/addProduct', (req, res) => {
+        const newProduct = req.body;
+        productCollection.insertOne(newProduct)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/checkout/:id', (req, res) => {
+        productCollection.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.post('/addOrders', (req, res) => {
+        const newOrders = req.body;
+        checkoutCollection.insertOne(newOrders)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/orders', (req, res) => {
+        checkoutCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.delete('/delete/:id', (req, res) => {
+        productCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => {
+                res.send(result.deletedCount > 0);
+            })
+    })
+
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
